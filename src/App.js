9f995dba5796a91d46a86911b7374f333d146d16
Gianna <gvc222@nyu.edu>
2024-06-17 11:22:08 -0700

    added pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -9,22 +9,23 @@ import Involved from './pages/Involved';
 import News from './pages/News';
 import Resources from './pages/Resources';
 import Contact from './pages/Contact';
+import Homepage from './pages/Homepage';
 
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Homepage}/>
-        <Route path="/projects" component={Projects}/>
-        <Route path="/impact" component={Impact}/>
-        <Route path="/get-involved" component={Involved}/>
-        <Route path="/news-and-events" component={News}/>
-        <Route path="/resources" component={Resources}/>
-        <Route path="/about-us" component={About}/>
-        <Route path="/contact-us" component={Contact}/>
-      </Switch>
+      <Routes>
+        <Route path="/" exact element={<Homepage />}/>
+        <Route path="/projects" element={<Projects />}/>
+        <Route path="/impact" element={<Impact />}/>
+        <Route path="/get-involved" element={<Involved />}/>
+        <Route path="/news-and-events" element={<News />}/>
+        <Route path="/resources" element={<Resources />}/>
+        <Route path="/about-us" element={<About />}/>
+        <Route path="/contact-us" element={<Contact />}/>
+      </Routes>
     </Router>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const Contact = () => {
     return(
-        <div>
-
+        <div className="page">
+            <h1>Contact Us</h1>
+            <p>Do you have any questions or suggestion? Reach us through the following options:</p>
+            <p>Phone number: +00000000000</p>
+            <p>Email: admin@bahaginan.org</p>
+            <iframe title="location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3812.753657700077!2d121.6118555760182!3d17.1335115837111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x338ff59d0e93cb05%3A0xa65ac5f3463f4970!2sLado%20Del%20Rio!5e0!3m2!1sen!2sus!4v1718646938578!5m2!1sen!2sus" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     )
 }

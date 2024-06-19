a5ff15795c9a93682d2e7a5b0b0fcc95b0ae29a5
Gianna <gvc222@nyu.edu>
2024-06-18 22:01:09 -0700

    added more text

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,16 +4,21 @@ const About = () => {
     return(
         <div className="page">
             <h1>About Us</h1>
-            <iframe src="https://photos.onedrive.com/share/38B72071DFA2319E!78818?cid=38B72071DFA2319E&resId=38B72071DFA2319E%2178818&authkey=%21AEIMD8r6gXHafhs&ithint=video&e=s4irgz&fbclid=IwAR2pMV71a9oaG7z_Anva9Br4VXbiKE7gT8wXBeshHR7pv_F2bOrWHzrU9FM" 
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowFullScreen
-            title="video"
-            width="100%"
-            height="500rem"
-            allowdownloads="false"
-            />
+            <h3>Our Story</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h3>Mission, Vision, and Values</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h3>Team</h3>
+                <ul className="team">
+                    <li><div className="profile">Corazon Calderon</div></li>
+                    <li><div className="profile">Vittorio</div></li>
+                    <li><div className="profile">Gianna</div></li>
+                </ul>
+            <h3>Partners and Sponsors</h3>
+            <ul>
+                <li><div className="profile">Roxas Isabela</div></li>
+            </ul>
+            
             <Footer />
         </div>
     )
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,6 +5,9 @@ const Projects = () => {
     return(
         <div className="page">
             <h1>Projects</h1>
+            <h3>Ongoing Projects</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h3>Past Projects</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <Footer />
         </div>
